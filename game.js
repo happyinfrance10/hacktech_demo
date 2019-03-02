43aebec922aaa2b6a74923442f72d915b492d4db
happyinfrance10 <alvinduong2000@gmail.com>
2019-03-02 14:50:20 -0800

    Added some normal distribution calculation and a function to test for resume screening in-game

--- game.js
+++ game.js
@@ -11,7 +11,36 @@ var model = {
     wit: 0
 }
 
-var pass_resume_screening = function(coding, resume, personality, wit, difficulty){
+function normal_distribution(mean, sd, value){
+  let z = (value-mean)/sd;
+  if (z < -6.5) {
+    return 0.0;
+  }
+
+  if (z > 6.5) {
+    return 1.0;
+  }
+
+  let factK = 1;
+  let sum = 0;
+  let term = 1;
+  let k = 0;
+  let loopStop = Math.exp(-23);
+
+  while(Math.abs(term) > loopStop) {
+    term = .3989422804 * Math.pow(-1,k) * Math.pow(z,k) / (2 * k + 1) / Math.pow(2,k) * Math.pow(z,k+1) / factK;
+    sum += term;
+    k++;
+    factK *= k;
+  }
+
+  sum += 0.5;
+
+  return sum;
+
+}
+
+function pass_resume_screening = function(coding, resume, personality, wit, difficulty){
   let score = coding*.2 + resume * .5 + personality *.15 + wit * .15;
    return (score > 900);
 }
