994b55600f9f6a990d502b5cfb0b186196a1e377
happyinfrance10 <alvinduong2000@gmail.com>
2019-03-02 23:06:33 -0800

    Fixed ending game message

--- game.js
+++ game.js
@@ -32,13 +32,15 @@ $(document).click(startGame);
 //Example code - define function to update model, then bind model to button
 //click event
 function button1() {    //leetcode
-  if(model.energy-150>=0 && model.happiness-25>=0){
+  if(model.energy-150>=0 && model.happiness-25>=0 && model.hoursLeft-3>=0){
     model.hoursLeft -= 3;
     model.coding +=(Math.floor(Math.random() * 5)+2);
     model.energy -=150;
     model.happiness -=25;
     checkVariables();
     //Update game logic here
+  } else if (model.hoursLeft-3<0){
+    document.getElementById("status").innerHTML = "You have no time to do this! Choose another task!";
   } else if (model.happiness-25<0){
     document.getElementById("status").innerHTML = "Spending too much time on career preparation can be a bad thing...";
   } else {
@@ -48,7 +50,7 @@ function button1() {    //leetcode
 $("#button1").click(button1);
 
 function button2() {
-  if(model.energy-50>=0 && model.happiness-20>=0){
+  if(model.energy-50>=0 && model.happiness-20>=0 && model.hoursLeft-3>=0){
     model.hoursLeft -= 3;
     model.connections +=(Math.floor(Math.random() * 5)+4);
     model.personality +=(Math.floor(Math.random() * 5));
@@ -56,6 +58,8 @@ function button2() {
     model.happiness -=20;
     checkVariables();
     //Update game logic here
+  } else if (model.hoursLeft-3<0){
+    document.getElementById("status").innerHTML = "You have no time to do this! Choose another task!";
   } else if (model.happiness-20<0){
     document.getElementById("status").innerHTML = "Spending too much time on career preparation can be a bad thing...";
   } else {
@@ -65,15 +69,19 @@ function button2() {
 $("#button2").click(button2);
 
 function button3() {  //nap
+  if(model.hoursLeft-3>=0){
     model.hoursLeft -= 3;
     model.energy +=500;
     checkVariables();
     //Update game logic here
+  } else {
+      document.getElementById("status").innerHTML = "You have no time to do this! Choose another task!";
+  }
 }
 $("#button3").click(button3);
 
 function button4() {    //project
-  if(model.energy-200>=0 && model.happiness-50>=0){
+  if(model.energy-200>=0 && model.happiness-50>=0 && model.hoursLeft-12>=0){
     model.hoursLeft -= 12;
     model.coding +=(Math.floor(Math.random() * 3)+1);
     model.resume +=(Math.floor(Math.random() * 7)+3);
@@ -81,6 +89,8 @@ function button4() {    //project
     model.energy -=200;
     checkVariables();
     //Update game logic here
+  } else if (model.hoursLeft-12<0){
+    document.getElementById("status").innerHTML = "You have no time to do this! Choose another task!";
   } else if (model.happiness-50<0){
     document.getElementById("status").innerHTML = "Spending too much time on career preparation can be a bad thing...";
   } else {
@@ -90,13 +100,15 @@ function button4() {    //project
 $("#button4").click(button4);
 
 function button5() {    //friend
-  if(model.energy-25>=0){
+  if(model.energy-25>=0 && model.hoursLeft-3>=0){
     model.hoursLeft -= 3;
     model.happiness +=500;
     model.personality +=(Math.floor(Math.random() * 6)+3);
     model.energy -=25;
     checkVariables();
     //Update game logic here
+  } else if (model.hoursLeft-3<0){
+    document.getElementById("status").innerHTML = "You have no time to do this! Choose another task!";
   } else {
     document.getElementById("status").innerHTML = "Do you really want to end up too exhausted to be productive soon?";
   }
@@ -158,6 +170,7 @@ function checkVariables(){
   if (model.hoursLeft === 0) {
     //game over stuff: if past a certain level, say congrats.
     //
+    console.log("MVP");
     if(pass_resume_screening(model.coding, model.resume, model.personality, model.connections)){
       document.getElementById("status").innerHTML = "Time up! You have worked very hard to become successful, and your efforts paid off with a job offer! Refresh to play again.";
     } else {
